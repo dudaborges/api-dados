dcf1715419e58005f5cd86be99ed07925f0ea4fa
MARIA EDUARDA PEREIRA BORGES <maria_ep_borges@estudante.sc.senai.br>
2024-06-21 20:01:16 -0300

    add temperature in db

--- services/backend/migrations/models/0_20240621224911_init.py
+++ services/backend/migrations/models/0_20240621224911_init.py
@@ -4,8 +4,9 @@ from tortoise import BaseDBAsyncClient
 async def upgrade(db: BaseDBAsyncClient) -> str:
     return """
         CREATE TABLE IF NOT EXISTS "pressuredata" (
-    "sensor_id" SERIAL NOT NULL PRIMARY KEY,
+    "id" SERIAL NOT NULL PRIMARY KEY,
     "pressure" INT,
+    "temperature" INT,
     "timestamp" TIMESTAMPTZ NOT NULL  DEFAULT CURRENT_TIMESTAMP
 );
 CREATE TABLE IF NOT EXISTS "aerich" (
--- services/backend/src/database/models.py
+++ services/backend/src/database/models.py
@@ -4,4 +4,5 @@ from tortoise import fields, models
 class PressureData(models.Model):
     id = fields.IntField(pk=True)
     pressure = fields.IntField(max_length=255, null=True)
+    temperature = fields.IntField(max_length=255, null=True)
     timestamp = fields.DatetimeField(auto_now=True)
